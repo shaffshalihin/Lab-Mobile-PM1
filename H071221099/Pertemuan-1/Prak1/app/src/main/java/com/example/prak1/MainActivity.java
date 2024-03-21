497967cb2cda820ed49c87d0035f82cce012a377
fathantsaqib <fathants12@gmail.com>
2024-03-21 15:34:17 +0800

    kumpultugasprak1

---
+++ H071221099/Pertemuan-1/Prak1/app/src/main/java/com/example/prak1/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.prak1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<String> dataList = new ArrayList<>(); // Daftar untuk menyimpan data
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editTextLainnya = findViewById(R.id.inputEditText);
+        Button buttonSubmit = findViewById(R.id.addButton);
+        LinearLayout listLayout = findViewById(R.id.listLayout);
+
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputText = editTextLainnya.getText().toString().trim();
+
+
+                dataList.add(inputText);
+                listLayout.removeAllViews();
+
+                for (String data : dataList) {
+                    TextView textView = new TextView(MainActivity.this);
+                    textView.setText(data);
+                    listLayout.addView(textView);
+                }
+                editTextLainnya.setText("");
+            }
+        });
+    }
+}
